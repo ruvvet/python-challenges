a0319f859cde0815e0fdb61f017613e975abf142
Jenny <finej01@gmail.com>
2021-01-07 15:24:28 -0500

    added soln

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,21 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+def calculator():
+    method = input('Pick one: +, -, *, /:  ')
+    numbers = input('input 2 numbers separated by a ,:  ')
+    a, b = map(lambda x: int(x), numbers.split(','))
+
+    if method == '+':
+        return a+b
+    elif method == '-':
+        return a-b
+    elif method == '*':
+        return a*b
+    elif method == '/':
+        return a//b
+
+
+print(calculator())
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,26 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+
+def deposit(bal):
+    amt = int(input('amt to deposit: $'))
+    return f'Your current balance: ${bal+amt}'
+
+
+def withdraw(bal):
+    amt = int(input('amt to withdraw: $'))
+    return f'Your current balance: ${bal-amt}'
+
+
+def bank():
+    balance = 4000
+    action = input('Display Balance, Withdraw, Deposit?: (B/W/D)').upper()
+    if action == 'B':
+        return f'Your current balance: ${balance}'
+    elif action == 'W':
+        return withdraw(balance)
+    elif action == 'D':
+        return deposit(balance)
+
+
+print(bank())
